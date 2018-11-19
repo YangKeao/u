774f4e559870db534dc6198ed294cc3554f256ad
Yang Keao <keao.yang@yahoo.com>
2018-11-19 18:45:08 +0800

    Add sample draw function

--- src/lib.rs
+++ src/lib.rs
@@ -22,10 +22,20 @@ pub fn create_application(backend: Backend) -> impl Application {
 }
 
 pub trait Application {
-    type Window;
+    type Window: Window;
     type WindowIdentifier;
     fn new() -> Self;
     fn create_window(&mut self, width: u16, height: u16) -> Self::WindowIdentifier;
     fn main_loop(&mut self);
     fn get_window(&mut self, id: Self::WindowIdentifier) -> &Self::Window;
+}
+
+pub struct Point {
+    x: i16,
+    y: i16,
+}
+
+pub trait Window {
+    type Application;
+    fn poly_point(&mut self, application: &Self::Application, points: &[Point]);
 }
